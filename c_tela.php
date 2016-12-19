89896e613c3d9789da74ad88d52f60166cc8241c
professordelphi <marcos.professordelphi@gmail.com>
2016-12-19 13:14:04 -0200

    etapa antes de mostrar para cliente

--- c_tela.php
+++ c_tela.php
@@ -15,7 +15,10 @@ function fillTitle($title)
                                                             <meta charset ='utf-8'>
 					<head>
 						<title>$title</title>
-						<link href='estilo.css' rel='stylesheet' type='text/css' />
+					
+						<link href='estilos/estilo.css' rel='stylesheet' type='text/css' />
+					
+						
 					</head> 
                                                                             <body>
 
@@ -42,14 +45,29 @@ function fillTitle($title)
 function showFormLogin($destino)
 {
 echo "
-              <form action='$destino' method='post'>
+              <form action='$destino' method='post' id='form'>
 
-<div class='lateral'></div>
-<input type='email' name='email' size='50' placeholder='Digite Email' />
-<br/>
-<div class='lateral'></div>
-<input type='phone' name='phone'    size='50' placeholder='Digite Telefone' />
+			 <div id='formInterno'> 
+			
+
+                    <div id='formInternoEmail'> 
+			
+<div  id='lateralEmail'>░  ▒ </div> <!--alt+176 e alt + 177 -->
+<input type='email' name='email' size='50' placeholder='  Email' id='txtEmail' />
+
+					</div>
+
+					<div id='formInternoPhone'> 
+					
+<div id='lateralPhone'>░  ▒ </div> <!--alt+176 e alt + 177 -->
+<input type='phone' name='phone'    size='50' placeholder='  Telefone' id='txtPhone' />
+
+					</div>
+					
+			<div id='formInternoButton'> 
 <input type='submit' name='btentrar' value='Entrar' id='btentrar'/>
+			</div>
+             </div>  <!--formInterno-->
 
               </form>
 
