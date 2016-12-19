0d4582c326686a28cbe3735aaf7760ff71c8e4e5
professordelphi <marcos.professordelphi@gmail.com>
2016-12-19 11:01:06 -0200

    envio dos arquivos html

---
+++ c_tela.php
@@ -0,0 +1,69 @@
+﻿<?php
+
+
+class Tela  {
+
+function fillTitle($title)
+{
+
+
+			echo "	
+                                                            <!doctype html>
+                                       
+                                                           <html lang='pt-br'>
+
+                                                            <meta charset ='utf-8'>
+					<head>
+						<title>$title</title>
+						<link href='estilo.css' rel='stylesheet' type='text/css' />
+					</head> 
+                                                                            <body>
+
+                                                                       <div id='geral'>	
+                                                                                            ";
+
+}
+
+	function showLogo(){
+		echo "
+					
+					
+					<section id='cima'>
+
+                                                                                          <div id='logo'></div>
+							
+				                </section>
+						
+							
+			";
+	}
+	
+
+function showFormLogin($destino)
+{
+echo "
+              <form action='$destino' method='post'>
+
+<div class='lateral'></div>
+<input type='email' name='email' size='50' placeholder='Digite Email' />
+<br/>
+<div class='lateral'></div>
+<input type='phone' name='phone'    size='50' placeholder='Digite Telefone' />
+<input type='submit' name='btentrar' value='Entrar' id='btentrar'/>
+
+              </form>
+
+";
+}
+
+
+
+	function close(){
+		echo "
+
+                                                                  </div>   <!--fecha o div geral-->
+				</body></html>
+		";
+	}
+}
+?>
---
+++ index.php
@@ -0,0 +1,9 @@
+﻿<?php
+include("c_tela.php");
+
+$objeto = new Tela;
+$objeto -> fillTitle("MAXNOTE");
+$objeto -> showLogo();
+$objeto -> showFormLogin("index.php");
+$objeto -> close();
+?>
